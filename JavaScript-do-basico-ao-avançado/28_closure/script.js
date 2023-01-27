65241392a78b039b53f6bf4333222cd0f506590f
RennanAtes <RennanAugustoBR@gmail.com>
2023-01-27 11:19:15 -0300

    Closure

--- JavaScript-do-basico-ao-avançado/28_closure/script.js
+++ JavaScript-do-basico-ao-avançado/28_closure/script.js
@@ -1,8 +1,24 @@
-function potencia(base, exp=2){
-    return Math.pow(base,exp);
+function lembrarSoma(x) {
+    return function(y) {
+        return x+ y;
+    }
 }
+let soma1 = lembrarSoma(2);
 
-console.log(potencia(2));
-console.log(potencia(2,2));
-console.log(potencia(2,4));
-console.log(potencia(2,3));
+console.log(soma1(5));
+
+let soma2 = lembrarSoma(2);
+
+console.log(soma2(7));
+
+function contador(i){
+    let cont = i;
+    let somarContador = function() {
+        console.log(cont);
+        cont++;
+    }
+    return somarContador;
+}
+let meuContador = contador(5);
+meuContador();
+meuContador();

ee946cd84a6183f3f055a4499a3fb98cc40df6b9
RennanAtes <RennanAugustoBR@gmail.com>
2023-01-27 10:53:29 -0300

    Escopos

---
+++ JavaScript-do-basico-ao-avançado/24_escopoAninhado/script.js
@@ -0,0 +1,12 @@
+let a = 10;
+function multplicar(x,y){
+    let a = x * y;
+    if (a > 10){
+        let a = 0;
+        console.log(a);
+    }
+    console.log(a);
+}
+console.log(a);
+
+multplicar(3,7);

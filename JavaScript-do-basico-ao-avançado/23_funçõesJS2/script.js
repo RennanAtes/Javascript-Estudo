a2ca93e24796a32ba93658bf7cd1ef6c048f0b6f
RennanAtes <RennanAugustoBR@gmail.com>
2023-01-27 10:44:20 -0300

    Funções

---
+++ JavaScript-do-basico-ao-avançado/23_funçõesJS2/script.js
@@ -0,0 +1,9 @@
+function imprimirNoConsole(Mensagem){
+    console.log(Mensagem);
+}
+function multiplicar(n1,n2){
+    console.log(n1*n2);
+}
+
+imprimirNoConsole("Olá Mundo!");  
+multiplicar(2,2);
